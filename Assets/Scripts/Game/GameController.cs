78dd20fef388e4e26be2d081d87c7e4a412f9d75
Chatchai Saratakij <CSaratakij@gmail.com>
2021-01-28 01:06:01 +0700

    Add : MainMenu UI

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance = null;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        MakeSingleton();
+    }
+
+    void MakeSingleton()
+    {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangeScene(SceneIndex sceneIndex)
+    {
+        SceneManager.LoadScene((int)sceneIndex);
+    }
+}
+
---
+++ Assets/Scripts/UI/UIMainMenuController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+public class UIMainMenuController : MonoBehaviour
+{
+    [SerializeField]
+    Button[] buttons;
+
+    enum ButtonIndex
+    {
+        Start,
+        HowToPlay,
+        Credit,
+        Quit
+    }
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        buttons
+            [(int)ButtonIndex.Start].
+            onClick.
+            AddListener(() => {
+                    GameController.Instance?.ChangeScene(SceneIndex.Level);
+        });
+
+        buttons
+            [(int)ButtonIndex.HowToPlay].
+            onClick.
+            AddListener(() => {
+                    //TODO
+        });
+
+        buttons
+            [(int)ButtonIndex.Credit].
+            onClick.
+            AddListener(() => {
+                    //TODO
+        });
+
+        buttons
+            [(int)ButtonIndex.Quit].
+            onClick.
+            AddListener(() => {
+                #if UNITY_EDITOR
+                    EditorApplication.isPlaying = false;
+                #else
+                    Application.Quit();
+                #endif
+        });
+    }
+}
+
