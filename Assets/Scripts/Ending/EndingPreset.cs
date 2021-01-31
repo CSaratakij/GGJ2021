1bc5bc7b6eb7634876e54314f7f28d163b1d9999
shinoyakame <shinoyakame02@gmail.com>
2021-01-31 16:39:08 +0700

    GameOver and Ending Sciprtable object

---
+++ Assets/Scripts/Ending/EndingPreset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EndingPreset", menuName = "Profile/Ending")]
+public class EndingPreset : ScriptableObject
+{
+    public int priority;
+    public string endingName;
+    public ConditionAction[] condition;
+    public Sprite sprite;
+    [TextArea(5, 5)] public string description;
+}
+
+
---
+++ Assets/Scripts/UI/UIGameOverController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIGameOverController : MonoBehaviour
+{
+    [Header("Ending UI")]
+    [SerializeField]
+    GameObject endingCanvas;
+
+    [SerializeField]
+    Image endingImage;
+
+    [SerializeField]
+    TextMeshProUGUI lblEnding;
+
+    [SerializeField]
+    TextMeshProUGUI txtDescription;
+
+    [SerializeField]
+    EndingPreset defaultEndingPreset;
+
+    private void Awake()
+    {
+        endingCanvas.SetActive(false);
+    }
+
+    private void Start()
+    {
+        EndingPreset endingPreset = GameController.Instance?.endingPreset;
+        if(endingPreset != null)
+        {
+            endingPreset = defaultEndingPreset;
+        }
+        endingImage.sprite = endingPreset.sprite;
+        lblEnding.text = endingPreset.endingName;
+        txtDescription.text = endingPreset.description;
+    }
+
+    public void BackToMainmenu()
+    {
+        GameController gameController = GameController.Instance;
+        if (gameController != null) {
+            gameController.ChangeScene(SceneIndex.MainMenu);
+        }
+        else
+        {
+            SceneManager.LoadScene((int)SceneIndex.MainMenu);
+        }
+    }
+}
